a5d9e11c2bc8c3cc4de10da7f412314cffa6f5f6
lucio-bytes <alucio.dev@gmail.com>
2026-03-16 00:27:19 -0300

    feat: add seo jsonld  config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://programa-reset.vercel.app/',
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
